78fedaf3e34907b1df798575f48c1fd03eac894b
thebestjb <thebestjb@gmail.com>
2019-01-13 22:32:50 +0100

    WIP - Interacting with Azure function and communicate with DB is OKAY.

---
+++ ParkingSpotsManager.Shared/Database/ParkingSpotsManagerDatabase.cs
@@ -0,0 +1,37 @@
+﻿using ParkingSpotsManager.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSpotsManager.Shared.Database
+{
+    public static class ParkingSpotsManagerDatabase
+    {
+        readonly static string _connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+        public static string TestMethod()
+        {
+            List<User> users = new List<User>();
+            using (SqlConnection connection = new SqlConnection(_connectionString)) {
+                var query = "select * from user_table";
+                var sqlCmd = new SqlCommand(query, connection);
+
+                connection.Open();
+                var reader = sqlCmd.ExecuteReader();
+
+                if (reader.HasRows) {
+                    while (reader.Read()) {
+                        var user = new User() {
+                            Email = reader.GetString(1)
+                        };
+                        users.Add(user);
+                    }
+                }
+            }
+
+            return users[0].Email;
+        }
+    }
+}
---
+++ ParkingSpotsManager.Shared/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingSpotsManager.Shared.Models
+{
+    class User
+    {
+        public string Email { get; set; }
+    }
+}
