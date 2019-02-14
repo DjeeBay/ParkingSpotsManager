d591ee41cdab783e430c2dd77c712bf64cdd94c7
JB <thebestjb@gmail.com>
2019-02-14 07:55:23 +0100

    WIP - can edit a parking.

--- ParkingSpotsManager/ParkingSpotsManager/ViewModels/ParkingEditPageViewModel.cs
+++ ParkingSpotsManager/ParkingSpotsManager/ViewModels/ParkingEditPageViewModel.cs
@@ -1,10 +1,17 @@
-﻿using ParkingSpotsManager.Shared.Models;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ParkingSpotsManager.Shared.Constants;
+using ParkingSpotsManager.Shared.Models;
+using Prism;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace ParkingSpotsManager.ViewModels
 {
@@ -17,9 +24,34 @@ namespace ParkingSpotsManager.ViewModels
             set { SetProperty(ref _currentParking, value); }
         }
 
+        public DelegateCommand<object> SaveParkingCommand { get; }
+
         public ParkingEditPageViewModel(INavigationService navigationService) : base (navigationService)
         {
+            SaveParkingCommand = new DelegateCommand<object>(OnSaveParkingCommandExecutedAsync, CanSaveParking);
+        }
 
+        private bool CanSaveParking(object arg)
+        {
+            return true;
+        }
+
+        private async void OnSaveParkingCommandExecutedAsync(object obj)
+        {
+            var url = new StringBuilder(APIConstants.ParkingREST).Append("/").Append(CurrentParking.Id).ToString();
+            var json = JObject.FromObject(CurrentParking);
+            var token = PrismApplicationBase.Current.Properties["authToken"].ToString();
+            using (var httpClient = new HttpClient()) {
+                try {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var response = await httpClient.PutAsync(url, new StringContent(json.ToString(), Encoding.UTF8, "application/json"));
+                    response.EnsureSuccessStatusCode();
+                    var content = await response.Content.ReadAsStringAsync();
+                    await NavigationService.NavigateAsync("ParkingListPage");
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
